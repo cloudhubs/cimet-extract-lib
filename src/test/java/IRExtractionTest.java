1c88413cd877a4e8aade2d8e33afdcd55e9f654f
Gabriel Goulis <ninjagabe2003@gmail.com>
2024-12-03 21:28:41 -0600

    Test

--- src/test/java/IRExtractionTest.java
+++ src/test/java/IRExtractionTest.java
@@ -6,7 +6,7 @@ import edu.university.ecs.lab.intermediate.create.services.IRExtractionService;
 public class IRExtractionTest {
 
     public static final String TEST_CONFIG_PATH = "/resources/test_config.json";
-    
+
     @Test
     void testCreate() {
         MicroserviceSystem newSystem = IRExtractionService.create(TEST_CONFIG_PATH);
